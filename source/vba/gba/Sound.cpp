9a56f15ed5a620564a7fc67ad49daf8750467b10
saulfabreg Wii VC Project <saulfabregamboa@outlook.com>
2022-04-11 12:56:20 -0500

    Reverted change of audio sample rate since it caused issues

--- source/vba/gba/Sound.cpp
+++ source/vba/gba/Sound.cpp
@@ -41,7 +41,7 @@ extern bool stopState;      // TODO: silence sound when true
 int const SOUND_CLOCK_TICKS_ = 167772; // 1/100 second
 
 static u16   soundFinalWave [1600];
-long  soundSampleRate    = 32000;
+long  soundSampleRate    = 44100;
 bool  soundInterpolation = true;
 bool  soundPaused        = true;
 float soundFiltering     = 0.5f;
